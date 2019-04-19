ffc5dd8490c0eeabaf291f746a108908b71a2f89
Mark Moore <moorem9@oregonstate.edu>
2019-04-19 17:41:37 -0500

    added the pages for the user to navigate. all pages are working. I changed the buttons to a form so that they would all run through a function on the way to the page. THis will allow access of the DB to load user names or passwords in to the html.

--- routes/users.js
+++ routes/users.js
@@ -43,6 +43,53 @@ router.get('/login', function(req, res){
 	res.render('login', {page: page});
 })
 
+// Route for user profile
+router.get('/usermyprofile', function(req, res){
+	var page = {
+		title: "My Profile"
+	}
+
+	res.render('usermyprofile', {page: page});
+})
+
+// Route for user to create an award for another person.
+router.get('/usercreateaward', function(req, res){
+	var page = {
+		title: "Create An Award"
+	}
+
+	res.render('usercreateaward', {page: page});
+})
+
+// Route for user to view previous awards given by him.
+router.get('/userviewawards', function(req, res){
+	var page = {
+		title: "View Awards Given"
+	}
+
+	res.render('userviewawards', {page: page});
+})
+
+
+// Route for user to reset users password
+router.get('/userresetpassword', function(req, res){
+	var page = {
+		title: "View Awards Given"
+	}
+
+	res.render('userresetpassword', {page: page});
+})
+
+// Route for user update their profile
+router.get('/usereditprofile', function(req, res){
+	var page = {
+		title: "View Awards Given"
+	}
+
+	res.render('usereditprofile', {page: page});
+})
+
+
 // Post route for new user registration
 router.post('/register', upload.single('signature'), [
 	check('email', 'Invalid Email').isEmail(), //check email format
