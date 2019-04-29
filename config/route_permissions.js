fb4ee185319313c03365ec7916403dc99c5a7acf
Rickey Patel <rickey90@gmail.com>
2019-04-29 00:02:22 -0500

    Implemented user account completion

--- config/route_permissions.js
+++ config/route_permissions.js
@@ -15,7 +15,13 @@ module.exports ={
     ensureUser: function(req, res, next){
         if(req.isAuthenticated()){
             if(!req.user.isAdmin){
-                return next();
+                if(req.user.accountComplete == 0){
+                    return next();
+                } else{
+                    req.flash('warning', "You must complete your registration before you can proceed.");
+                    res.redirect('/users/completeaccount');
+                    return;
+                }
             }else{
                 req.flash('danger', "Admins can't go there!");
                 res.redirect('/users/admin/adminmenu');
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,47 @@ const passport = require('passport');
 const { check, validationResult } = require('express-validator/check'); //middleware for express-validator
 const bcrypt = require('bcryptjs'); //middleware for encrypting pw
 const routePermission = require(`../config/route_permissions.js`);
+const multer = require('multer');
+const fs = require('fs');
+
+// Middleware setup for multer
+const storage = multer.diskStorage({
+	destination: function(req, file, cb){
+		cb(null, './public/signatures'); // specify storage location for user signature images
+	},
+	filename: function(req, file, cb){
+		cb(null, Date.now() +  file.originalname); // specify file name to give signature
+	}
+})
+
+// Filter file setup for storing signatures
+const fileFilter = function(req, file, cb){
+	if(file.mimetype === 'image/png' || file.mimetype === 'image/jpeg'){ //store only jpeg and png images, reject all others 
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+}
+
+const upload = multer({storage: storage, fileFilter: fileFilter});
+
+// router.get('/checkaccountstatus', routePermission.ensureUser, function(req, res){
+// 	//check if this account needs to complete registration
+// 	if(req.user.accountComplete == 0){ //if it is already complete, then go to user main page
+// 		res.redirect('/users/usermainmenu');
+// 	} else { //if it is not complete, then user must complete before proceeding
+// 		req.flash('warning', 'You must complete your registration before you can proceed.');
+// 		res.redirect('/users/completeaccount');
+// 	}
+// })
+
+router.get('/completeaccount', function(req, res){
+	var page = {
+		title: "Complete Registration"
+	}
+
+	res.render('completeuseraccount', {page: page});
+})
 
 router.get('/usermainmenu', routePermission.ensureUser, function(req, res){
 	var page = {
@@ -127,5 +168,55 @@ router.post('/userresetpassword', routePermission.ensureUser, [
 	})
 
 })
+
+// Post route for completing user registration
+router.post('/completeaccount', upload.single('signature'), [
+	check('password', 'Password must be at least 6 characters long').isLength({min: 6}), //check password length
+	check('password2').custom((value, { req }) => {  //ensure password confirmation matches
+		if (value !== req.body.password) {
+			throw new Error('Password confirmation does not match password');
+		} else return value;
+	}),
+	check('secQ1').custom((value, { req }) => { //ensure security questions are not the same
+		if (value === req.body.secQ2){
+			throw new Error('Security questions must be different');
+		} else return value;
+	})
+], function(req, res){
+
+	const err = validationResult(req); //get the errors
+	const { fName, lName, email, title, department, password, secQ1, secQ1Ans, secQ2, secQ2Ans } = req.body; //bring in body parameters 
+
+	//if there is an error, display the error messages 
+	if(!err.isEmpty()){ 
+		var errors = err.array();
+		fs.unlink(req.file.path, (er) => { // delete the signature file
+			if(er){ console.log(err); }
+			res.render('completeuseraccount', {
+				errors: errors, 
+				page: {title: 'Register'}
+			});
+		})
+	} else{ //update user account to in DB
+		bcrypt.genSalt(10, function(err, salt){ //first hash the new password
+			bcrypt.hash(password, salt, function(err, hash){
+				if(err){ console.log(err); }
+				var mysql = req.app.get('mysql');
+				var sql = "UPDATE user SET fname=?, lName=?, password=?, title=?, department=?, secQ1=?, secQ1Ans=?, secQ2=?, secQ2Ans=?, signature =?, accountComplete=? WHERE userId=?";
+				var inserts = [fName, lName, hash, title, department, secQ1, secQ1Ans, secQ2, secQ2Ans, req.file.path, 0, req.user.userId];
+				sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+					if(error){
+						console.log(JSON.stringify(error));
+						res.write(JSON.stringify(error));
+						res.end();
+					} else{
+						req.flash('success', 'You have successfully completed your registration!')
+						res.redirect('/users/usermainmenu');
+					}
+				})
+			})
+		})
+	}
+});
 
 module.exports = router;
